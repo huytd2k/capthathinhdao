cf527573fcfee06ba39342867c879ecfc66ded5e
Huy tran <huy.td183768@sis.hust.edu.vn>
2021-01-13 00:03:39 +0700

    Update: get rand

--- be/src/caption/caption.controller.ts
+++ be/src/caption/caption.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { PrivateRoute } from 'src/core/decorators/private-route.decorator';
 import { CaptionService } from './caption.service';
 import { CreateCaptionDto } from './dtos/create-caption.dto';
+import { CaptionType } from './enums/caption-type.enum';
 
 @Controller('caption')
 export class CaptionController {
@@ -13,5 +14,11 @@ export class CaptionController {
   @ApiBearerAuth()
   async create(@Body() createCaptionDto: CreateCaptionDto) {
     return await this.captionService.create(createCaptionDto);
+  }
+
+  @Get()
+  @ApiQuery({ name: 'type', enum: CaptionType, required: false })
+  async getRandom(@Query('type') type: CaptionType) {
+    return await this.captionService.findRandom(type);
   }
 }
--- be/src/caption/caption.service.ts
+++ be/src/caption/caption.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { MongoRepository } from 'typeorm';
 import { CreateCaptionDto } from './dtos/create-caption.dto';
 import { Caption } from './entites/caption.entity';
+import { CaptionType } from './enums/caption-type.enum';
 
 @Injectable()
 export class CaptionService {
   constructor(
     @InjectRepository(Caption)
-    private readonly captionRepo: Repository<Caption>,
+    private readonly captionRepo: MongoRepository<Caption>,
   ) {}
 
   async create(createCaptionDto: CreateCaptionDto) {
     return await this.captionRepo.save(new Caption(createCaptionDto));
+  }
+
+  async findRandom(type: CaptionType) {
+    return await this.captionRepo
+      .aggregateEntity([{ $match: { type } }, { $sample: { size: 1 } }])
+      .toArray()
+      .then((x) => x[0]);
   }
 }
